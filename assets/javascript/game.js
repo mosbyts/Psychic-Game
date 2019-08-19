79c66c549bf55e47643768fbd9c3584567acd87c
Tianna Mosby <mosbyts@vcu.edu>
2019-08-18 22:12:52 -0400

    Added simple JavaScript to get the game started. Currently all action is in the console.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
     var Wins = 0;
     var Losses = 0;
-    var Ties = 0;
+    var Guesses = 5;
 
     var computerGuesses = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
@@ -9,4 +9,25 @@
             toUpperCase();
 
         console.log(userGuess);
+
+        var computerGuess = computerGuesses[Math.floor(Math.random()*computerGuesses
+        .length)];
+
+        console.log(computerGuess);
+        console.log("------------");
+
+        if(userGuess==computerGuess){
+            Wins++;
+            console.log("You have won!");
+            console.log("Wins: " + Wins);
+        }
+
+        if(userGuess!==computerGuess){
+            Losses++;
+            Guesses--;
+            console.log("Wins: " + Wins)
+            console.log("Losses: " + Losses);
+            console.log("Guesses left: " + Guesses);
+        }
+
     }
